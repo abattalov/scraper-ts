e2a70523d890f9d8c38658875640b364ff11a129
abattalov <abattalov14@gmail.com>
2025-11-13 15:32:28 -0600

    added concurrency

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
-import { crawlPage } from "./crawl";
+import { crawlSiteAsync } from "./crawl";
 
 async function main() {
-    if (process.argv.length < 3) {
-        console.log("no website provided");
-        process.exit(1);
-    }
-    if (process.argv.length > 3) {
-        console.log("too many arguments provided");
-        process.exit(1);
-    }
-    const baseURL = process.argv[2];
+  if (process.argv.length < 3) {
+    console.log("no website provided");
+    process.exit(1);
+  }
+  if (process.argv.length > 3) {
+    console.log("too many arguments provided");
+    process.exit(1);
+  }
+  const baseURL = process.argv[2];
 
-    console.log(`starting crawl of: ${baseURL}...`);
+  console.log(`starting crawl of: ${baseURL}...`);
 
-    await crawlPage(baseURL);
+  const pages = await crawlSiteAsync(baseURL);
 
-    process.exit(0);
+  console.log(pages);
+
+  process.exit(0);
 }
 
 main();
